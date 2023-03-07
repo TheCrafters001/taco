392eb66ca8b205ff9044d4aeb3c920d3c91ef687
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2023-03-07 02:00:08 -0500

    taco

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-taco","displayName":"taco","link":{"linkType":"LinkTypePage","href":"#!page-taco"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-speed","displayName":"speed","link":{"linkType":"LinkTypePage","href":"#!page-speed"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
